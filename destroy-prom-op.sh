4ff7e648cad75a03caa70df1fe67c17627f4fef4
vmdn23 <victormdnguyen88@gmail.com>
2019-10-07 23:16:09 -0700

    script to destroy prom operator

---
+++ destroy-prom-op.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+helm delete my-release
+kubectl delete crd prometheuses.monitoring.coreos.com
+kubectl delete crd prometheusrules.monitoring.coreos.com
+kubectl delete crd servicemonitors.monitoring.coreos.com
+kubectl delete crd podmonitors.monitoring.coreos.com
+kubectl delete crd alertmanagers.monitoring.coreos.com
